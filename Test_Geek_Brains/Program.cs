89be1ca327f54346f6b42231cfe114c2a3216817
Polina <polykabackaya@gmail.com>
2022-07-24 16:22:28 +0300

    add Create and Print methods

--- Test_Geek_Brains/Program.cs
+++ Test_Geek_Brains/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите текст. Для разделения слов используйте пробелы (в том числе, после знаков препинания): ");
+string text = Console.ReadLine();
+Console.WriteLine("Введите количество символов. Слова с таким или меньшим количеством символов будут выведены на экран: ");
+int Characters = int.Parse(Console.ReadLine());
+
+string[] CreateArray(string N)
+{
+    string[] array = N.Split(' ');
+
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("'");
+
+        Console.Write(array[i] + "'");
+
+        Console.Write(" ");
+    }
+}
