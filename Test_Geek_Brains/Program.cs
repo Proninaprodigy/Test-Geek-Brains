771e05133019c8c2ec01385c8e63b2fa6cd441d2
Polina <polykabackaya@gmail.com>
2022-07-24 16:44:31 +0300

    add main method

--- Test_Geek_Brains/Program.cs
+++ Test_Geek_Brains/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+string[] CreateArrayCharacters(string[] array, int Quantity, int size)
+
+{
+
+string[] ReceivedArray = new string[size];
+
+int n = 0;
+
+for (int m = 0; m < array.Length; m++)
+
+{
+
+if (array[m].Length <= Quantity)
+
+{
+
+ReceivedArray[n] = array[m];
+
+n++;
+
+}
+
+}
+
+return ReceivedArray;
+
+}
+
+CreateArray(text);
+
+PrintArray(CreateArray(text));
+
+Console.Write("--> ");
+
+int count = Counter(CreateArray(text), Characters);
+
+CreateArrayCharacters(CreateArray(text), Characters, count);
+
+PrintArray(CreateArrayCharacters(CreateArray(text), Characters, count));
+
+Console.WriteLine();
